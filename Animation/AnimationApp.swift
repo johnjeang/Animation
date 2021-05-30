354c0fc936177ffa07f9e62f7d86ddf1a1a89a73
johnjeang <johnjeang@gmail.com>
2021-05-29 19:39:45 -0500

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by John Jeang on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
